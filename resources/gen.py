8db465916e819799c26c4091250eeb56dbe84767
Microservices dev tips <vip.digitive@gmail.com>
2024-02-14 14:30:29 +1000

    fixed typo

--- resources/gen.py
+++ resources/gen.py
@@ -3,13 +3,13 @@ import secrets
 import string
 import crypt
 
-def gen_passwrod(length):
+def gen_password(length):
     password = ''.join((secrets.choice(string.ascii_letters + string.digits + string.punctuation) for i in range(length)))
     salt = ''.join((secrets.choice(string.ascii_letters) for i in range(2)))
     return (password, salt)
 
 for i in range(8):
     for j in range(20):
-        password, salt = gen_passwrod(i+1)
+        password, salt = gen_password(i+1)
         hash = crypt.crypt(password, salt)
         print(password + ' ' + hash)
